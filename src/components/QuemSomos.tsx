57811b092d39998a9422a0113b99d6b2f7ead87d
Rafael Vicente <rafaelpv15@gmail.com>
2025-01-16 14:56:59 -0300

    fix mudanca de banner

--- src/components/QuemSomos.tsx
+++ src/components/QuemSomos.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 
-import quemSomos from '/public/quem-somos-img.png';
+import bannerWeb from '/public/banner web.svg';
+import bannerMobile from '/public/mobile.svg';
 
 type QuemSomosProps = {
   className?: string;
@@ -8,13 +9,10 @@ type QuemSomosProps = {
 
 export default function QuemSomos(props: QuemSomosProps) {
   return (
-    <div className={`${props.className}`}>
-      <Image
-        src={quemSomos}
-        alt="Quem somos photo"
-        className="w-full md:w-1/2"
-      />
-      <div className="w-full md:w-1/2 p-4">
+    <div className={`${props.className} relative`}>
+      <Image className='md:inline hidden w-[90%] h-auto' src={bannerWeb} alt='Banner gatinho'/>
+      <Image className='inline md:hidden' src={bannerMobile} alt='Banner gatinho'/>
+      <div className="w-full md:w-1/2 p-4 xl:-ml-80 md:-ml-56">
         <h1 className="text-4xl md:text-7xl text-center text-mainColor font-extrabold pb-10">
           QUEM SOMOS
         </h1>
