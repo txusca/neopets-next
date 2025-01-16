2cb2b544a52e523843bbc00ace5536a210969ebd
Rafael Vicente <rafaelpv15@gmail.com>
2025-01-16 14:26:56 -0300

    fix btn agende e tamanho container

--- src/components/AgendeAtendimento.tsx
+++ src/components/AgendeAtendimento.tsx
@@ -19,11 +19,11 @@ export default function AgendeAtendimento(props: AgendeAtendimentoProps) {
       />
       <div className="bg-background flex items-center justify-center xl:rounded-[34px] flex-wrap p-5 overflow-x-hidden">
         <Image
-          className="md:ml-16 xl:order-1 order-2"
+          className="xl:order-1 order-2"
           src={cachorro}
           alt="Cachorro de pé"
         />
-        <div className="flex flex-col items-start xl:order-2 order-1">
+        <div className="flex flex-col items-center xl:order-2 order-1">
           <h3 className="text-[28px] text-mainColor font-medium text-center">
             Agende seu atendimento
           </h3>
@@ -32,15 +32,15 @@ export default function AgendeAtendimento(props: AgendeAtendimentoProps) {
             atendimento do seu pet com apenas um clique. Nossa equipe está
             pronta para cuidar com carinho e dedicação.
           </p>
-          <Link href={'https://wa.me/+5538997413703'} target='_blank'>
-            <div className="flex md:w-[294px] md:h-[84px] w-72 h-20 bg-green-400 rounded-2xl text-white justify-center items-center text-3xl font-normal cursor-pointer">
+          <Link className='mt-3' href={'https://wa.me/+5538997413703'} target='_blank'>
+            <div className="flex w-72 h-20 bg-green-400 rounded-2xl text-white justify-center items-center text-3xl font-normal cursor-pointer">
               Fale conosco
-              <Image className="ml-[30px]" src={wpp} alt="Whatsapp icon" />
+              <Image className="ml-3" src={wpp} alt="Whatsapp icon" />
             </div>
           </Link>
         </div>
         <Image
-          className="relative ml-auto -mt-[-200px] mr-6 self-end xl:inline hidden md:order-3"
+          className="relative ml-auto -mt-[-200px] self-end xl:inline hidden md:order-3"
           src={gatinho}
           alt="Gatinho deitado"
         />
